800490652be4544a69d51a67aaf640ac08c17b88
HyeminNoh <shrmffkfk@naver.com>
2020-02-05 00:53:15 +0900

    Add PostsRepository.java Post클래스로 DB를 접근하게 해줄 JpaRepository 생성

---
+++ src/main/java/com/hyemin/noh/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.hyemin.noh.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
