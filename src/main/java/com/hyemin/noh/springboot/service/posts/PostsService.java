b3bbb459225cc24e7a88e6eec27e92156a49d04d
HyeminNoh <shrmffkfk@naver.com>
2020-02-05 17:50:30 +0900

    Update PostsService.java 수정, 조회 추가

--- src/main/java/com/hyemin/noh/springboot/service/posts/PostsService.java
+++ src/main/java/com/hyemin/noh/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.hyemin.noh.springboot.service.posts;
 
+import com.hyemin.noh.springboot.domain.posts.Posts;
 import com.hyemin.noh.springboot.domain.posts.PostsRepository;
+import com.hyemin.noh.springboot.web.dto.PostsResponseDto;
 import com.hyemin.noh.springboot.web.dto.PostsSaveRequestDto;
+import com.hyemin.noh.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()-> new
+                        IllegalArgumentException("해당 사용자가 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
