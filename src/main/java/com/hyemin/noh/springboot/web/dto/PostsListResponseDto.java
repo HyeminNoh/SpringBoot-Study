7bce76a148c5c9c01e6798c233a10f1eb9677bd6
HyeminNoh <shrmffkfk@naver.com>
2020-02-06 16:56:25 +0900

    PostsListResponseDto

---
+++ src/main/java/com/hyemin/noh/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.hyemin.noh.springboot.web.dto;
+
+import com.hyemin.noh.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
