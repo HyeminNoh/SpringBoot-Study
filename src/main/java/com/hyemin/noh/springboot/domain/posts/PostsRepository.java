4354d12f72a0239b73c6ea059c463808944c5ba4
HyeminNoh <shrmffkfk@naver.com>
2020-02-06 16:58:49 +0900

    Posts 전체 조회 쿼리문 추가

--- src/main/java/com/hyemin/noh/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/hyemin/noh/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.hyemin.noh.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    //SpringDataJpa 제공 기본메소드를 활용해도 되지만 Query가 가독성이 좋음
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
